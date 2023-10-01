5e3a23f53e55716591a32fca3ff01bd36581c068
vthyrachel <vthyrachel@naver.com>
2023-10-01 20:22:39 +0900

    basic(~chap2)

--- src/main/java/hello/springstart/controller/HelloController.java
+++ src/main/java/hello/springstart/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.springstart.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -16,5 +18,39 @@ public class HelloController {
 
         // 빌드하는거.. clean build 하면 빌드 파일 지우고 다시 생성함
         // clean이 빌드 파일 지우는거
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    // 객체 반환, @ResponseBody 하면 거의 무조건 json 형식으로 넘어감
+    // 객체를 반환한다는게 api 방법의 핵심?인 것 같네요
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello {
+        private String name;
+
+        // alt + Fn + Insert = generate(for getter & setter)
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
